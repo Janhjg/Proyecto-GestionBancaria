ac7576ffc6a19f0c059441eacef2abb65fc2b6fb
Booleano <sergio23.tp@gmail.com>
2025-12-12 11:08:44 +0100

    Añadiendo función crear usuario

--- banco.py
+++ banco.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
         opcion = input("Selecciona una opción: ")
 
         if opcion == "1":
-            create_user_fn() # insertar función de crear usuario
+            funciones.crear_usuario() # insertar función de crear usuario
         elif opcion == "2":
-            user = login_fn()  # insertar función de login
+            user = ()  # insertar función de login
             if user:
                 print(f"\nBienvenido, {user['nombre']}!")
                 return user    # Devuelves el usuario para pasar al menú principal de operaciones
